f898675651aadbba935c71077f2199e0011c6cbf
jswon0803 <jswon@kfpa.or.kr>
2024-12-03 16:19:41 +0900

    'tester'

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv
 load_dotenv()
 
-st.set_page_config(page_title="JR. Board Chat", page_icon=":computer:")   
+st.set_page_config(page_title="Insu Chatbot", page_icon=":computer:")   
 
 # Sidebar 의 header 및 markdown 글자 색깔 & expander 배경색 지정
 st.markdown(
@@ -116,7 +116,7 @@ st.markdown(
 st.markdown(
     """
     <div class="main-header">
-         KFPA 복지 기준 검색기<span>📖</span>
+         KFPA 규정사례 모음집<span>📖</span>
     </div>
     """,
     unsafe_allow_html=True
@@ -130,33 +130,22 @@ with st.sidebar:
     st.markdown('<div class="sidebar-title">JR. Board Chatbot</div>', unsafe_allow_html=True)
 
     # About 헤더
-    st.markdown('<div class="sidebar-header">About</div>', unsafe_allow_html=True)
-    st.markdown("복지와 관련된 기준 기반으로 질문에 답변합니다")
+    st.markdown('<div class="sidebar-header">소개</div>', unsafe_allow_html=True)
+    st.markdown("KFPA 규정검색 도우미")
 
     # Document list 헤더
-    st.markdown('<div class="sidebar-header">Document list</div>', unsafe_allow_html=True)
+    st.markdown('<div class="sidebar-header">질문예제</div>', unsafe_allow_html=True)
     st.markdown(
         """
         <ul class="sidebar-list">
-            <li>가족수당지급기준</li>
-            <li>국내교육연수출장비지급기준</li>
-            <li>비연고지단신근무자교통보조금지급기준</li>
-            <li>임직원대출운영기준</li>
-            <li>자기계발지원기준</li>
-            <li>자녀양육수당 지급기준</li>
-            <li>중식수당 지급기준</li>
-            <li>직원 차량유지비 등 지급기준</li>
-            <li>직원 경조금지급기준</li>
-            <li>체력단련비 지급기준</li>
-            <li>출퇴근보조금지급기준</li>
-            <li>피복관리기준</li>
-            <li>협회가 필요로 하는 분야의 자격 인정 종목 기준</li>
+            <li>xxx관련 규정 알려줘</li>
+
         </ul>
         """,
         unsafe_allow_html=True
     )
-    st.image('KV.png')
-    st.image('영문시그니처(소).jpg')
+    # st.image('KV.png')
+    # st.image('영문시그니처(소).jpg')
     
 @st.cache_resource(ttl="1h")
 def get_faiss_db():
@@ -205,10 +194,10 @@ def display_retrieved_documents(documents):
             source = os.path.basename(doc.metadata["source"])
             st.markdown(f"** from {source}**")
 
-# db = get_faiss_db()
-# retriever = db.as_retriever(search_type="similarity",
-#                             search_kwargs={'k':5, 'fetch_k':8},
-#                             )     
+db = get_faiss_db()
+retriever = db.as_retriever(search_type="similarity",
+                            search_kwargs={'k':5, 'fetch_k':8},
+                            )     
 
 # Setup memory for contextual conversation
 msgs = StreamlitChatMessageHistory()
